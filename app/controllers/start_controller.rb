92b32793b4c4ab0240bdf7a9ff1dea7ee1bdf2e8
HagiwaraK <28046322+HagiwaraK@users.noreply.github.com>
2021-02-24 22:14:38 +0900

    main page of 1st ver.

---
+++ app/controllers/start_controller.rb
@@ -0,0 +1,5 @@
+class StartController < ApplicationController
+  def master
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-
-  resources :articles
+  get 'start/master' => 'start#master'
 
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
