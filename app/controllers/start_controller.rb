33228725f084416e6cf1b7134a36a4551a9d4847
java-chip <takuyam0111@yahoo.co.jp>
2021-03-08 04:07:39 +0900

    send user id parameters to master page

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -1,5 +1,11 @@
 class StartController < ApplicationController
+  protect_from_forgery
+
   def master
+  	@east_user = User.find(params[:east].to_i)
+  	@south_user = User.find(params[:south].to_i)
+  	@west_user = User.find(params[:west].to_i)
+  	@north_user = User.find(params[:north].to_i)
   end
 end
 
--- app/models/wind.rb
+++ app/models/wind.rb
@@ -1,3 +1,3 @@
 class Wind < ApplicationRecord
-	enum wind_list: {"0": "東", "1": "南", "2": "西", "3": "北"}
+	enum wind_list: {"0": ["east", "東"], "1": ["south", "南"], "2": ["west", "西"], "3": ["north", "北"]}
 end
