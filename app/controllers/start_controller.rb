079829f7e91fa33302ee45719fef5ca05da5a865
java-chip <takuyam0111@yahoo.co.jp>
2021-03-09 01:27:17 +0900

    show json of half_round, game and user_scores

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -2,10 +2,29 @@ class StartController < ApplicationController
   protect_from_forgery
 
   def master
-  	@east_user = User.find(params[:east].to_i)
-  	@south_user = User.find(params[:south].to_i)
-  	@west_user = User.find(params[:west].to_i)
-  	@north_user = User.find(params[:north].to_i)
+  	@first_leader = User.find(params[:east].to_i)
+  	@second_leader = User.find(params[:south].to_i)
+  	@third_leader = User.find(params[:west].to_i)
+  	@fourth_leader = User.find(params[:north].to_i)
+  	@half_round = HalfRound.new
+  	@half_round.save!
+  	init_score = 25000
+  	init_chip = 0
+  	@first_leader_score = UserScore.new(
+  		user_id:@first_leader.id, point:init_score, chip:init_chip, half_round_id:@half_round.id
+  	)
+  	@second_leader_score = UserScore.new(
+  		user_id:@second_leader.id, point:init_score, chip:init_chip, half_round_id:@half_round.id
+  	)
+  	@third_leader_score = UserScore.new(
+  		user_id:@third_leader.id, point:init_score, chip:init_chip, half_round_id:@half_round.id
+  	)
+  	@fourth_leader_score = UserScore.new(
+  		user_id:@fourth_leader.id, point:init_score, chip:init_chip, half_round_id:@half_round.id
+  	)
+  	@game = Game.new(
+  		honba:0, round:"east", half_round_id: @half_round.id
+  		)
   end
 end
 
